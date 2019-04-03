e56a4eeea9e6a9d1294fd2599a9819f5b7cfb8f8
AurelijaZuba <auryte00@hotmail.com>
2019-04-03 17:48:51 +0100

    Passing test for get conservative party names

--- src/main/java/com/codurance/ElectionResults.java
+++ src/main/java/com/codurance/ElectionResults.java
@@ -1,5 +1,8 @@
 package com.codurance;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ElectionResults {
 
     public String electionTransformer(String input) {
@@ -11,16 +14,22 @@ public class ElectionResults {
         for(var i = 1; i < results.length - 1; i += 2)
         {
             var polingVote = results[i];
-            var partyCode = results[i + 1];
+            var partyCode = results[i + 1].trim();
 
-            actualResult = " ||" + getFullPartyName(partyCode) + " |" + polingVote;
+            actualResult += " || " + getFullPartyName(partyCode) + " | 100.00%" ;
         }
 
         return actualResult;
     }
 
 
-    private String getFullPartyName(String result) {
-        return null;
+    private String getFullPartyName(String partyCode) {
+        Map map=new HashMap();
+        map.put("C", "Conservative Party");
+
+
+        return (String)map.get(partyCode);
     }
+
+    //TODO add exception for the argument at later date for above
 }
