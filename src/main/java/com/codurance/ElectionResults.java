4af0f54387b6a519e2a59bf0f30a16e0e267e314
AurelijaZuba <auryte00@hotmail.com>
2019-04-04 09:07:10 +0100

    Failing test for second line constituency name

--- src/main/java/com/codurance/ElectionResults.java
+++ src/main/java/com/codurance/ElectionResults.java
@@ -1,7 +1,10 @@
 package com.codurance;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.IntStream;
 
 public class ElectionResults {
 
@@ -9,27 +12,49 @@ public class ElectionResults {
 
         String[] results = input.split(",");
         String constituencyName = results[0];
+        var actualResult = constituencyName;
 
-        String actualResult = constituencyName;
-        for(var i = 1; i < results.length - 1; i += 2)
-        {
-            var polingVote = results[i];
-            var partyCode = results[i + 1].trim();
+        for (var newLine : input.split(System.lineSeparator())) {
+            String[] result = newLine.split(",");
 
-            actualResult += " || " + getFullPartyName(partyCode) + " | 100.00%" ;
+            for (var i = 1; i < result.length - 1; i += 2) {
+
+                int polingVote = Integer.parseInt(result[i].trim());
+                var partyCode = result[i + 1].trim();
+
+//                var s = IntStream.of(polingVote).sum();
+//                var calculator = (double)(polingVote / s) ;
+
+
+                actualResult += " || " + getFullPartyName(partyCode) + " | 100%";
+
+            }
+
         }
-
         return actualResult;
     }
 
 
+    private String voteCalculator(String polingVote) {
+
+
+        return null;
+    }
+
     private String getFullPartyName(String partyCode) {
         Map map=new HashMap();
         map.put("C", "Conservative Party");
-
+        map.put("LD", "Liberal Democrats");
+        map.put("L", "Labour Party");
+        map.put("UKIP", "UKIP");
+        map.put("G", "Green Party");
+        map.put("Ind", "Independent");
 
         return (String)map.get(partyCode);
     }
 
+
     //TODO add exception for the argument at later date for above
+
+
 }
--- src/test/java/com/codurance/ElectionResultsShould.java
+++ src/test/java/com/codurance/ElectionResultsShould.java
@@ -27,7 +27,12 @@ public class ElectionResultsShould {
     static Stream<Arguments> singleLineTestCases() {
         return Stream.of(
                 arguments("Cardiff West,", "Cardiff West"),
-                arguments("Cardiff West, 11014, C", "Cardiff West || Conservative Party | 100.00%")
+                arguments("Cardiff West, 11014, C", "Cardiff West || Conservative Party | 100%"),
+                arguments("Cardiff West, 11014, C, 17803, L, 4923, UKIP, 2069, LD\n" +
+                          "Islington South & Finsbury, 22547, L, 9389, C, 4829, LD, 3375, UKIP, 3371, G, 309, Ind",
+                          "Cardiff West || Conservative Party | 30.76% || Labour Party | 49.72% || UKIP | 13.75% || Liberal Democrats | 5.78%\n" +
+                          "Islington South & Finsbury || Labour Party | 51.45% || Conservative Party | 21.43% || Liberal Democrats | 11.02% || " +
+                                  "UKIP | 7.70% || Green Party | 7.69% || Independent | 0.71%")
         );
     }
 }
