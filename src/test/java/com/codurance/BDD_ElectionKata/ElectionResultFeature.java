39305db4e5d0d6ac974d9fbba43fa51e4230e355
AurelijaZuba <auryte00@hotmail.com>
2019-04-05 09:43:55 +0100

    Integrated cucumber for BDD

---
+++ src/test/java/com/codurance/BDD_ElectionKata/ElectionResultFeature.java
@@ -0,0 +1,23 @@
+package com.codurance.BDD_ElectionKata;
+
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class ElectionResultFeature {
+    //BDD Version
+
+
+    @Given("Small Input")
+    public void get_translated_election_results_from_given_input() {
+    }
+
+    @When("I ask to convert the election result")
+    public void iAskToConvertTheElectionResult() {
+    }
+
+    @Then("I should get formatted statistics")
+    public void iShouldGetFormattedStatistics() {
+    }
+}
